4333ce7ac545889eacf318c311a83ea803e4c96f
Sindhura Gollamudi <gollamudisindhura1@gmail.com>
2026-01-03 21:24:42 -0500

    add Header component with title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,10 @@ export default function Header(){
     return (
         <header className="header">
 
+        
             <h1>IP Address Tracker</h1>
+        
+        
         </header>
     )
 }
